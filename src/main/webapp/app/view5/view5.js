569af87e19c7eadde48f3b73755b036470a09bd6
nickl <nickl@10.50.131.14>
2016-11-01 10:20:06 +0100

    Initial setup of project

---
+++ src/main/webapp/app/view5/view5.js
@@ -0,0 +1,15 @@
+'use strcit';
+
+angular.module('myApp.view5', ['ngRoute'])
+
+        .config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'app/view5/view5.html',
+    controller: 'View5Ctrl',
+    controllerAs : 'ctrl'
+  });
+}])
+
+        .controller('View5Ctrl', function(){
+            
+});
